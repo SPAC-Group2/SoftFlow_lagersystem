9f76c7500a617687a46a771c5f7c4bbe6ce65254
Line Phoebe Wienke <linewienke+github@gmail.com>
2026-03-12 09:35:37 +0100

    Server satup + shared folder
    with node, install express
    for at køre det node index.ts

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (_req: any, res: { send: (arg0: string) => void }) => {
+  res.send('Hello World! and goodbye')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
