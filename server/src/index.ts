ab156c3f5511d1ba287339236b4a49181740a643
Yorl <94957768+DataFromBelow@users.noreply.github.com>
2026-03-13 11:13:37 +0100

    REST

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
-const express = require('express')
+import express from "express";
+import { routes } from "./features/routes";
+
+
 const app = express()
 const port = 3000
 
-app.get('/', (_req: any, res: { send: (arg0: string) => void }) => {
+app.use(express.json());
+/*app.get('/', (_req: any, res: { send: (arg0: string) => void }) => {
   res.send('Hello World! and goodbye')
-})
-
+})*/
+app.use("/", routes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
