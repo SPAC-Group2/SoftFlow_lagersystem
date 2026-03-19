4fa81a8146566b3ad38be6b48b30d75f03208dcf
Line Phoebe Wienke <linewienke+github@gmail.com>
2026-03-19 14:38:25 +0100

    filter fungere på produkter

--- server/src/controllers/ProductsController.ts
+++ server/src/controllers/ProductsController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { getExampleData, getExampleData3 } from "../services/exampleService";
+import {ProductFilter, parseProductFilter} from "../utils/ProductFilters";
 import { getAllProducts } from "../services/productService";
 
 export async function getProducts(req: Request, res: Response) {
-    const data = await getAllProducts();
+
+    const filters = parseProductFilter(req.query)
+    const data = await getAllProducts(filters);
+
     res.json(data);
 }
 
--- server/src/services/productService.ts
+++ server/src/services/productService.ts
@@ -1,12 +1,46 @@
 import pool from '../db/db_module'; 
 //import {Product} from "@shared/types";
 import {mapToProductList} from "../shared/mappers/ProductMapper";
+import { ProductFilter } from '../utils/ProductFilters';
 
 
-export async function getAllProducts() {
-  const query = 'SELECT * FROM GetProducts';
+export async function getAllProducts(filters : ProductFilter) {
+  let query = 'SELECT * FROM GetProducts';
+  console.log(filters.instock)
+  const conditions: string[] = [];
+  const values: any[] = [];
+  if(filters.category !== undefined){
+    values.push(filters.category)
+    conditions.push(`category ILIKE $${values.length}`) 
+  }
+  if (filters.instock !== undefined){
+    console.log(filters.instock)
+    if(filters.instock == false){
+      conditions.push("stock <= 0")
+    }
+    else{
+      conditions.push("stock >= 1")
+    }
+  }
+    if (conditions.length > 0) {
+      query += " WHERE " + conditions.join(" AND ");
+    }
+  if (filters.page !== undefined){
+    if(filters.page < 1){
+      filters.page = 1;
+    }
+    if(filters.pagesize == undefined || filters.pagesize < 1){
+      filters.pagesize = 10;
+    }
+    const OFFSET = (filters.page - 1) * filters.pagesize 
+    query += " LIMIT " + filters.pagesize + " OFFSET " + OFFSET;
+    console.log(query)
+  }
+
   try {
-    const result = await pool.query(query);
+    console.log(query)
+    const result = await pool.query(query, values);
+    
     return mapToProductList(result.rows);
   } catch (error) {
     console.error('Error executing query:', error);
@@ -14,13 +48,3 @@ export async function getAllProducts() {
   }
 }
 
-export async function getProductsFromDBByPage(take: number, skip: number) {
-  const query = 'SELECT * FROM ProductsView LIMIT  $1  OFFSET $2;';
-  const values = [take, skip]
-  try {
-    const result = await pool.query(query, values);
-    return result.rows;
-  } catch (error) {
-    console.error('Error executing query:', error);
-    throw error;
-  }}
